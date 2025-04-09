5ce30a0eb0706159822d6069d3f29cd08b452201
prabhmeet-kira <prabhmeetmail@gmail.com>
2025-04-10 02:33:07 +0530

    Styled Hero section

--- app/page.js
+++ app/page.js
@@ -1,12 +1,11 @@
+import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="mt-30"> 
-    <Button variant="destructive">Site in Production</Button>
-
-    </div>
-    
+      <HeroSection /> 
+    </div>   
   );
 }
---
+++ components/hero.jsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { Button } from "@/components/ui/button"; 
+import Image from "next/image";
+import Link from "next/link"; 
+
+const HeroSection = () => {
+  return (
+    <div className="pb-20 px-4"> 
+        <div className="container mx-auto text-center">
+
+            <h1 className="text-3xl md:text-6xl lg:text-[95px] pb-6 gradient gradient-title">
+                Your Financial Clarity <br/> Powered by AI
+            </h1>
+
+            <p className="text-xl text-gray-600 mb-8 max-w-2.5xl mx-auto">
+                Master your money. No guesswork.<br/>
+                AI-driven finance tracking that puts you in full control—track smarter, spend wiser, grow faster.
+            </p>
+
+            <div className="flex justify-center space-x-4 pb-6">
+                <Link href="/dashboard">
+                    <Button size="lg" className="px-8">
+                        Try It Now! 
+                    </Button>
+                </Link>
+
+                <Link href="https://github.com/prabhmeet-kira/MoneyMap">
+                    <Button size="lg" variant='outline' className="px-8">
+                        Explore the Code →
+                    </Button>
+                </Link>
+            </div>
+
+            <div>
+                <Image src="/banner.jpg" width={1280} height={720} alt="banner image" className="rounded-lg shadow-4xl border mx-auto" priority />
+            </div>
+
+
+
+
+
+
+
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
